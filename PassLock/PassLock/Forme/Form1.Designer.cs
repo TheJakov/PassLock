f32bbda1b1d3ba1008dcc7b2c22e8fb24fd497c5
Fabijan <37190757+fkraljic@users.noreply.github.com>
2018-12-01 22:48:10 +0100

    Stvorena sqlite datoteka
    Omogućeno spajanje SQLite datoteke. Ruši se na odabiru datoteke, putanja ne valja.

--- PassLock/PassLock/Forme/Form1.Designer.cs
+++ PassLock/PassLock/Forme/Form1.Designer.cs
@@ -29,10 +29,8 @@
         private void InitializeComponent()
         {
             this.labelaNaslov = new System.Windows.Forms.Label();
-            this.textBoxKorisnickoIme = new System.Windows.Forms.TextBox();
-            this.textBoxLozinka = new System.Windows.Forms.TextBox();
             this.buttonPrijaviSe = new System.Windows.Forms.Button();
-            this.buttonRegistrirajSe = new System.Windows.Forms.Button();
+            this.buttonNovaBaza = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // labelaNaslov
@@ -45,53 +43,35 @@
             this.labelaNaslov.TabIndex = 0;
             this.labelaNaslov.Text = "PassLock";
             // 
-            // textBoxKorisnickoIme
-            // 
-            this.textBoxKorisnickoIme.Location = new System.Drawing.Point(92, 85);
-            this.textBoxKorisnickoIme.Name = "textBoxKorisnickoIme";
-            this.textBoxKorisnickoIme.Size = new System.Drawing.Size(131, 22);
-            this.textBoxKorisnickoIme.TabIndex = 1;
-            // 
-            // textBoxLozinka
-            // 
-            this.textBoxLozinka.Location = new System.Drawing.Point(92, 113);
-            this.textBoxLozinka.Name = "textBoxLozinka";
-            this.textBoxLozinka.Size = new System.Drawing.Size(131, 22);
-            this.textBoxLozinka.TabIndex = 2;
-            // 
             // buttonPrijaviSe
             // 
-            this.buttonPrijaviSe.Location = new System.Drawing.Point(108, 141);
+            this.buttonPrijaviSe.Location = new System.Drawing.Point(108, 98);
             this.buttonPrijaviSe.Name = "buttonPrijaviSe";
             this.buttonPrijaviSe.Size = new System.Drawing.Size(103, 28);
             this.buttonPrijaviSe.TabIndex = 3;
-            this.buttonPrijaviSe.Text = "Prijavi se";
+            this.buttonPrijaviSe.Text = "Otvori bazu";
             this.buttonPrijaviSe.UseVisualStyleBackColor = true;
-            this.buttonPrijaviSe.Click += new System.EventHandler(this.buttonPrijaviSe_Click);
             // 
-            // buttonRegistrirajSe
+            // buttonNovaBaza
             // 
-            this.buttonRegistrirajSe.Location = new System.Drawing.Point(108, 190);
-            this.buttonRegistrirajSe.Name = "buttonRegistrirajSe";
-            this.buttonRegistrirajSe.Size = new System.Drawing.Size(103, 28);
-            this.buttonRegistrirajSe.TabIndex = 4;
-            this.buttonRegistrirajSe.Text = "Registriraj se";
-            this.buttonRegistrirajSe.UseVisualStyleBackColor = true;
-            this.buttonRegistrirajSe.Click += new System.EventHandler(this.buttonRegistrirajSe_Click);
+            this.buttonNovaBaza.Location = new System.Drawing.Point(110, 150);
+            this.buttonNovaBaza.Name = "buttonNovaBaza";
+            this.buttonNovaBaza.Size = new System.Drawing.Size(103, 28);
+            this.buttonNovaBaza.TabIndex = 4;
+            this.buttonNovaBaza.Text = "Nova baza";
+            this.buttonNovaBaza.UseVisualStyleBackColor = true;
+            this.buttonNovaBaza.Click += new System.EventHandler(this.buttonNovaBaza_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(311, 253);
-            this.Controls.Add(this.buttonRegistrirajSe);
+            this.ClientSize = new System.Drawing.Size(321, 271);
+            this.Controls.Add(this.buttonNovaBaza);
             this.Controls.Add(this.buttonPrijaviSe);
-            this.Controls.Add(this.textBoxLozinka);
-            this.Controls.Add(this.textBoxKorisnickoIme);
             this.Controls.Add(this.labelaNaslov);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Leave += new System.EventHandler(this.Form1_Leave);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,10 +80,8 @@
         #endregion
 
         private System.Windows.Forms.Label labelaNaslov;
-        private System.Windows.Forms.TextBox textBoxKorisnickoIme;
-        private System.Windows.Forms.TextBox textBoxLozinka;
         private System.Windows.Forms.Button buttonPrijaviSe;
-        private System.Windows.Forms.Button buttonRegistrirajSe;
+        private System.Windows.Forms.Button buttonNovaBaza;
     }
 }
 
--- PassLock/PassLock/Forme/Form1.cs
+++ PassLock/PassLock/Forme/Form1.cs
@@ -15,45 +15,37 @@ namespace PassLock
 {
     public partial class Form1 : Form
     {
-        private SQLiteConnection sQLiteConnection;
-
         public Form1()
         {
             InitializeComponent();
             this.Text = "PassLock";
-
-            string path = Path.GetDirectoryName(Application.ExecutablePath) + @"\PassLockDB.db";
-            sQLiteConnection = new SQLiteConnection("Data Source=" + path);
-            sQLiteConnection.Open();
         }
 
-        private void buttonRegistrirajSe_Click(object sender, EventArgs e)
+        private void buttonNovaBaza_Click(object sender, EventArgs e)
         {
-            RegistrirajSe registrirajSe = new RegistrirajSe();
-            registrirajSe.ShowDialog();
-        }
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "SQLite file|*.sqlite";
+            saveFileDialog.Title = "Save an SQLite File";
+            saveFileDialog.ShowDialog();
 
-        private void buttonPrijaviSe_Click(object sender, EventArgs e)
-        {
-            SQLiteCommand cmd = new SQLiteCommand();
-            cmd.Connection = sQLiteConnection;
+            if (saveFileDialog.FileName != "")
+            {
+                SQLiteConnection.CreateFile(saveFileDialog.FileName + ".sqlite");
+                System.IO.FileStream fs =
+                   (System.IO.FileStream)saveFileDialog.OpenFile();
+                
 
-            string korisnickoIme = textBoxKorisnickoIme.Text;
-            string lozinka = textBoxLozinka.Text;
+                SQLiteConnection konekcija = new SQLiteConnection("Data Source="+ fs + "/"+ saveFileDialog.FileName + ".sqlite;Version=3;");
+                konekcija.Open();
 
-            cmd.CommandText = "SELECT * FROM korisnik WHERE korime='" + korisnickoIme + "' AND lozinka='" + lozinka+"'";
+                string sql = "CREATE TABLE podaci (id INTEGER PRIMARY KEY UNIQUE NOT NULL, lozinka TEXT NOT NULL, lozinkaSHA256 TEXT NOT NULL)";
+                
+                SQLiteCommand command = new SQLiteCommand(sql, konekcija);
+                command.ExecuteNonQuery();
+                konekcija.Close();
 
-            int brojRedova;
-            string test = "";
-            SQLiteDataReader reader = cmd.ExecuteReader();
-
-            this.Text = test;
-            sQLiteConnection.Close();
-        }
-
-        private void Form1_Leave(object sender, EventArgs e)
-        {
-            sQLiteConnection.Close();
+                fs.Close();
+            }
         }
     }
 }
